5f1d1ab87a88be16524955c0799409ddc9b6fb12
yagiri000 <yohakuari@gmail.com>
2017-11-29 22:17:54 +0900

    フォルダ名とプロジェクト名を一致．不要なフォルダを削除

--- PostEffectTest2/Main.cpp
+++ PostEffectTest2/Main.cpp
@@ -67,10 +67,10 @@ int WINAPI wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, 
 
         AdjustWindowRect(&rc, WS_OVERLAPPEDWINDOW, FALSE);
 
-        HWND hwnd = CreateWindowEx(0, L"Direct3D_Win32_Game1WindowClass", L"Direct3D Win32 Game1", WS_OVERLAPPEDWINDOW,
+        HWND hwnd = CreateWindowEx(0, L"Direct3D_Win32_Game1WindowClass", L"2DSpriteDrawTest", WS_OVERLAPPEDWINDOW,
             CW_USEDEFAULT, CW_USEDEFAULT, rc.right - rc.left, rc.bottom - rc.top, nullptr, nullptr, hInstance,
             nullptr);
-        // TODO: Change to CreateWindowEx(WS_EX_TOPMOST, L"Direct3D_Win32_Game1WindowClass", L"Direct3D Win32 Game1", WS_POPUP,
+        // TODO: Change to CreateWindowEx(WS_EX_TOPMOST, L"Direct3D_Win32_Game1WindowClass", L"2DSpriteDrawTest", WS_POPUP,
         // to default to fullscreen.
 
         if (!hwnd)
