562e648e2e230a1b7e4ce30caeeb86711faad017
yagiri000 <yohakuari@gmail.com>
2018-04-30 22:54:12 +0900

    反応拡散系を実装

--- PostProcessTest/PSCopy.hlsl
+++ PostProcessTest/PSCopy.hlsl
@@ -10,58 +10,55 @@ struct VSInputTx
 
 float4 main(VSInputTx pin) : SV_Target
 {
-    float WIDTH_RATE = 1.0 / 1280.0 * 0.5;
-    float HEIGHT_RATE = 1.0 / 720.0 * 0.5;
-    float DOWN_RATE = HEIGHT_RATE * 0.01;
+    float WIDTH_RATE = 0.5 / 1280.0;
+    float HEIGHT_RATE = 0.5 / 720.0;
 
-    //float4 color = Texture.Sample(Sampler, pin.TexCoord) * 0.2;
-    float4 colorG = 0.0;
-    colorG += Texture.Sample(Sampler, pin.TexCoord + float2(WIDTH_RATE, 0.0 - DOWN_RATE)) * 0.25;
-    colorG += Texture.Sample(Sampler, pin.TexCoord + float2(-WIDTH_RATE, 0.0 - DOWN_RATE)) * 0.25;
-    colorG += Texture.Sample(Sampler, pin.TexCoord + float2(0.0, HEIGHT_RATE - DOWN_RATE)) * 0.25;
-    colorG += Texture.Sample(Sampler, pin.TexCoord + float2(0.0, -HEIGHT_RATE - DOWN_RATE)) * 0.25;
-    colorG.r = clamp(colorG.r, 0, 1);
+    float4 base = Texture.Sample(Sampler, pin.TexCoord);
+    
+    float4 color = Texture.Sample(Sampler, pin.TexCoord) * 0.2;
+    float dr = sin(base.b * 10) * -2.0 + 2.0;
+    color += Texture.Sample(Sampler, pin.TexCoord + float2(WIDTH_RATE, HEIGHT_RATE)) * 0.3;
+    color += Texture.Sample(Sampler, pin.TexCoord + float2(-WIDTH_RATE, HEIGHT_RATE)) * 0.3;
+    color += Texture.Sample(Sampler, pin.TexCoord + float2(-WIDTH_RATE, -HEIGHT_RATE)) * 0.3;
+    color += Texture.Sample(Sampler, pin.TexCoord + float2(WIDTH_RATE, -HEIGHT_RATE)) * 0.3;
+    color -= Texture.Sample(Sampler, pin.TexCoord + float2(WIDTH_RATE * dr, HEIGHT_RATE * dr)) * 0.1;
+    color -= Texture.Sample(Sampler, pin.TexCoord + float2(-WIDTH_RATE * dr, HEIGHT_RATE * dr)) * 0.1;
+    color -= Texture.Sample(Sampler, pin.TexCoord + float2(-WIDTH_RATE * dr, -HEIGHT_RATE * dr)) * 0.1;
+    color -= Texture.Sample(Sampler, pin.TexCoord + float2(WIDTH_RATE * dr, -HEIGHT_RATE * dr)) * 0.1;
+
+    float4 colorG = Texture.Sample(Sampler, pin.TexCoord + float2(0.005, 0.0)) * 0.2;
+    float dg = sin(base.r * 10) * -1.0 + 2.0;
+    colorG += Texture.Sample(Sampler, pin.TexCoord + float2(WIDTH_RATE, 0.0)) * 0.3;
+    colorG += Texture.Sample(Sampler, pin.TexCoord + float2(-WIDTH_RATE, 0.0)) * 0.3;
+    colorG += Texture.Sample(Sampler, pin.TexCoord + float2(0.0, HEIGHT_RATE)) * 0.3;
+    colorG += Texture.Sample(Sampler, pin.TexCoord + float2(0.0, -HEIGHT_RATE)) * 0.3;
+    colorG -= Texture.Sample(Sampler, pin.TexCoord + float2(WIDTH_RATE * dg, 0.0)) * 0.1;
+    colorG -= Texture.Sample(Sampler, pin.TexCoord + float2(-WIDTH_RATE * dg, 0.0)) * 0.1;
+    colorG -= Texture.Sample(Sampler, pin.TexCoord + float2(0.0, HEIGHT_RATE * dg)) * 0.1;
+    colorG -= Texture.Sample(Sampler, pin.TexCoord + float2(0.0, -HEIGHT_RATE * dg)) * 0.1;
+
+    float4 colorB = Texture.Sample(Sampler, pin.TexCoord) * 0.2;
+    float d = sin(base.g * 10) * 1.0 + 2.0;
+    colorB += Texture.Sample(Sampler, pin.TexCoord + float2(WIDTH_RATE, 0.0)) * 0.3;
+    colorB += Texture.Sample(Sampler, pin.TexCoord + float2(-WIDTH_RATE, 0.0)) * 0.3;
+    colorB += Texture.Sample(Sampler, pin.TexCoord + float2(0.0, HEIGHT_RATE)) * 0.3;
+    colorB += Texture.Sample(Sampler, pin.TexCoord + float2(0.0, -HEIGHT_RATE)) * 0.3;
+    colorB -= Texture.Sample(Sampler, pin.TexCoord + float2(WIDTH_RATE * d, 0.0)) * 0.1;
+    colorB -= Texture.Sample(Sampler, pin.TexCoord + float2(-WIDTH_RATE * d, 0.0)) * 0.1;
+    colorB -= Texture.Sample(Sampler, pin.TexCoord + float2(0.0, HEIGHT_RATE * d)) * 0.1;
+    colorB -= Texture.Sample(Sampler, pin.TexCoord + float2(0.0, -HEIGHT_RATE * d)) * 0.1;
+    
+    
+    color.r = clamp(color.r, 0, 1);
     colorG.g = clamp(colorG.g, 0, 1);
-    colorG.b = clamp(colorG.b, 0, 1);
-    colorG.rgb *= 0.99;
-    //colorG.gb *= 0.9;
+    colorB.b = clamp(colorB.b, 0, 1);
+    color.r *= 0.999;
+    colorG.g *= 0.999;
+    colorB.b *= 0.999;
+    //colorG.rgb *= 0.96;
+    //colorB.rgb *= 0.95;
 
-    float4 color_ = Texture.Sample(Sampler, pin.TexCoord);
-    
-    float4 color = Texture.Sample(
-    Sampler,
-    pin.TexCoord 
-    + float2(sin(color_.g * 10) * 0.001, cos(color_.b * 10) * 0.001)
-    + float2(
-    cos(cos(pin.TexCoord.y * 5) * 10) * WIDTH_RATE,
-    cos(sin(pin.TexCoord.x * 5) * 10) * HEIGHT_RATE
-    )
-    );
 
-    float4 color2 = Texture.Sample(
-    Sampler,
-    pin.TexCoord 
-    + float2(sin(color_.b * 10) * 0.001, cos(color_.r * 10) * 0.001)
-    + float2(
-    cos(sin(pin.TexCoord.y * 10) * 10) * WIDTH_RATE,
-    cos(cos(pin.TexCoord.x * 10) * 10) * HEIGHT_RATE
-    )
-    );
 
-    float4 color3 = Texture.Sample(
-    Sampler,
-    pin.TexCoord 
-    + float2(sin(color_.r * 10) * 0.001, cos(color_.g * 10) * 0.001)
-    + float2(
-    cos(cos(pin.TexCoord.y * 3) * 30) * WIDTH_RATE,
-    sin(cos(pin.TexCoord.x * 3) * 30) * HEIGHT_RATE
-    )
-    );
-    
-    color.r = color.r - 0.2 / 255.0;
-    color2.g = color2.g - 0.5 / 255.0;
-    color3.b = color3.b - 0.5 / 255.0;
-    color = float4(color.r, color2.g, color3.b, 1.0);
-
-    return color * 0.9 + colorG * 0.1;
+    return float4(color.r, colorG.g, colorB.b, color.a);
 }
